816698eb14be526095570513b2eefd77d5994aa8
Yulia S <contremoi@yandex.ru>
2019-07-08 23:48:20 +0300

    new pages

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var car = {
+			make: "WV",
+			type: "Polo",
+			color: "blue",
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ["seaat1", 
+			"seat 2",
+			"seat 3"],
+			turnedOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log("turn car " + isOn)
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				}
+				else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
