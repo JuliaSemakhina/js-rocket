14befbd217c3b978247b1595ffe30d4da4e190ce
Yulia S <contremoi@yandex.ru>
2019-07-09 00:54:31 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById("cool").className;
+
+	if (currentClassName == "cool") {
+		document.getElementById("cool").className = "cool red";
+	}
+	else {
+		document.getElementById("cool").className = "cool";
+	}
+	document.getElementById("cool").className = "cool red";
+}
+
+
 var car = {
 			make: "WV",
 			type: "Polo",
